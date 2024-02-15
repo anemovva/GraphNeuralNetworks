5e531e7f6237629c0aa97cfc995acba544647e7c
Aneesh <anemovva@gmail.com>
2024-02-14 23:20:27 -0600

    Added scaffolding for adding the GATLSTM model to the project.

---
+++ gatlstm.py
@@ -0,0 +1,12 @@
+import pytorch.nn as nn
+import pytorch as torch
+from gat import GAT
+
+class GATLSTM(nn.Module):
+    def __init__(self, ):
+        # Make sure to get info re: graph and store it so it can be passed to others
+        # TODO: Define the model
+    
+    def forward(self, ):
+        # TODO: Implement the forward pass
+        return
---
+++ utils.py
@@ -0,0 +1,6 @@
+class GraphInfo():
+    
+    def __init__(self) -> None:
+        pass
+    
+    # TODO: Make this class determine which nodes are adjacent to each other (based on distance), make adjacency matrix, etc.
