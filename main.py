7420414c886851fb94c063779b89e4f1eb79d620
Aneesh <anemovva@gmail.com>
2024-02-19 11:56:43 -0600

    Formatting

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
-import torch.nn as nn
-from gat import GAT
-from torch_geometric.data import Data
+# from torch_geometric.data import Data
 from torch_geometric.data import DataLoader
 # from torch_geometric.datasets import Planetoid
 import torch_geometric.datasets.citation_full as citation_full
 import torch
-import dgl
+# import dgl
 
-def main():
+import torch.nn as nn
+from gat import GAT
+
+
+def main() -> None:
+    """Main function for training GAT on the CORA dataset.
+    """
     # Get CORA dataset from DGL
 
    # Create DataLoader
     # open ./data/cora/cora.content
     # open ./data/cora/cora.cites
     # Create DataLoader
-    
-    
+
+
     dataset = citation_full.CitationFull(root='./data/cora', name='cora')
-    
+
     dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
-    
-    
-    
+
+
+
     # Create model, try setting to cuda, cpu if not available
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = GAT(dataset.num_features, 1).to(device)
-    
+
     # Define loss function and optimizer
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
@@ -33,11 +37,11 @@ def main():
     print("Training")
 
     # Training loop
-    
+
     initial = 0
     best = 100000000000
-    
-    
+
+
     for epoch in range(1000):
         total_loss = 0
         for data in dataloader:
@@ -45,7 +49,7 @@ def main():
             # Convert targets to float
             data.y = data.y.float()
             optimizer.zero_grad()
-            output = model(data)
+            output = model.forward(data)
             output = output.view(-1)
             loss = criterion(output, data.y)
             loss.backward()
@@ -56,7 +60,6 @@ def main():
         if initial == 0:
             initial = total_loss
         print(f"Epoch {epoch+1}: Loss = {total_loss} (Initial = {initial}, Best = {best})")
-    
     return
 
 main()
